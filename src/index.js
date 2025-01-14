7160e358352aae7e91116105abb2caff0346688a
Paarth Monga <paarthmonga28@gmail.com>
2025-01-15 00:55:55 +0530

    added body parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express=require('express')
+const bodyParser=require('body-parser')
 
 const serverConfig = require('./config/serverConfig')
 const connectDB = require('./config/dbConfig')
 
 const app=express()
+
+// we require deserializer based on different types of input formats
+
+// logic to parse the data that is input in json form
+app.use(bodyParser.json())
+// if input body is in text
+app.use(bodyParser.text())
+// if input given in url-encoded in postman
+app.use(bodyParser.urlencoded())
 
 app.listen(serverConfig.PORT,async ()=>{
     await connectDB();
