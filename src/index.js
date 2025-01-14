2007f3ba9763bfc9ffaef6096d69b8b9e52a5bcc
Paarth Monga <paarthmonga28@gmail.com>
2025-01-15 00:11:40 +0530

    mongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express=require('express')
 
 const serverConfig = require('./config/serverConfig')
+const connectDB = require('./config/dbConfig')
 
 const app=express()
 
-app.listen(serverConfig.PORT,()=>{
+app.listen(serverConfig.PORT,async ()=>{
+    await connectDB();
     console.log(`server started at port ${serverConfig.PORT}`)
 })
+
